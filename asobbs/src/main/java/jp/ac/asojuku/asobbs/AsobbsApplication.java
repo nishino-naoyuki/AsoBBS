15946cdfcd910af4d32ab7e72524f52840aad997
nishino-naoyuki <nishino@asojuku.ac.jp>
2019-01-28 18:17:02 +0900

    初めてのアップ

---
+++ asobbs/src/main/java/jp/ac/asojuku/asobbs/AsobbsApplication.java
@@ -0,0 +1,14 @@
+package jp.ac.asojuku.asobbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsobbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsobbsApplication.class, args);
+	}
+
+}
+
