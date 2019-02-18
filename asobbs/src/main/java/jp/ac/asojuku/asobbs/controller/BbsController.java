c10e7e1215ceeb2764554eec75a8321ca2c508ac
nishino-naoyuki <nishino@asojuku.ac.jp>
2019-02-18 21:56:59 +0900

    tuika

--- asobbs/src/main/java/jp/ac/asojuku/asobbs/controller/BbsController.java
+++ asobbs/src/main/java/jp/ac/asojuku/asobbs/controller/BbsController.java
@@ -1,12 +1,16 @@
 package jp.ac.asojuku.asobbs.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import jp.ac.asojuku.asobbs.form.BbsInputForm;
+import jp.ac.asojuku.asobbs.form.RoomInputForm;
 
 @Controller
 @RequestMapping(value= {"/bbs"})
@@ -18,6 +22,16 @@ public class BbsController {
 		BbsInputForm form = new BbsInputForm();
         mv.setViewName("/input_bbs");
         mv.addObject("bbsInputForm",form);
+        return mv;
+    }
+
+	@RequestMapping(value= {"/confirm"}, method=RequestMethod.POST)
+    public ModelAndView confirm(ModelAndView mv,
+    		@Valid BbsInputForm bbsInputForm,
+			BindingResult bindingResult) {
+		
+        mv.setViewName("/input_bbs");
+        mv.addObject("bbsInputForm",bbsInputForm);
         return mv;
     }
 }
